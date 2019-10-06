4e9d92257ea739d506af95dd1c79d9f99cb52217
Alexander <nesson@mail.ru>
2019-10-06 22:10:54 +0300

    123

--- src/lesson1/Cat.java
+++ src/lesson1/Cat.java
@@ -4,8 +4,7 @@ public class Cat {
 
     private static String jumping;
     private static Object Cat;
-//    private final String jumping;
-    
+
     public static void main(String[] args) {
         Cat cat = new Cat("Прыгает");
         System.out.println(Cat);
